fd51b67343852bf53fbb87675c051d02b8de95b5
Youssef Abdulaziz <azizy5203@gmail.com>
2025-02-16 13:47:04 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  interface user {
+    name: string;
+    age: Number;
+  }
+  const x: user = {
+    name: "ff",
+    age: 4,
+  };
+</script>
+
+<template>
+  <div>
+    <h1 class="text-7xl">Home</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
